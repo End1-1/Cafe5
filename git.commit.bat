7872c73839e8cdd3f0fc76aebcdbf9bc0918de23
End1-1 <end1_1@yahoo.com>
2020-05-25 11:15:00 +0400

    Last db

--- git.commit.bat
+++ git.commit.bat
@@ -1,4 +1,4 @@
-mysqldump -uroot -posyolia cafe5 > c:\Projects\Cafe5\db.cafe5.sql
+c:\Projects\build\Cafe5\debug\mysqldump.exe -uroot -posyolia cafe5 > c:\Projects\Cafe5\db.cafe5.sql
 git add .
 git commit
 git push
