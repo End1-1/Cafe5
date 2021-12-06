6a0dc860d6d8f5c9495a830ffbb53de3aa3640bd
user <end1_1@yahoo.com>
2021-12-06 16:23:34 +0400

    db

--- git.commit.bat
+++ git.commit.bat
@@ -1,4 +1,4 @@
-c:\Projects\build\Cafe5\debug\mysqldump.exe -uroot -posyolia cafe5 > c:\Projects\Cafe5\db.cafe5.sql
+c:\Projects\Cafe5\mariadb-dump.exe -uroot -posyolia cafe5 > c:\Projects\Cafe5\db.cafe5.sql
 rar a -df -ma5 -m5 -ep c:\Projects\Cafe5\db.cafe5.rar c:\Projects\Cafe5\db.cafe5.sql
 git add .
 git commit
