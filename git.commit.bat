bc3b818049e606d1455134c4d4c87c75f2937d8d
user <end1_1@yahoo.com>
2022-03-24 23:43:03 +0400

    Service5 with db

--- git.commit.bat
+++ git.commit.bat
@@ -1,4 +1,5 @@
 c:\Projects\Cafe5\mariadb-dump.exe -uroot -posyolia cafe5 > c:\Projects\Cafe5\db.cafe5.sql
+del c:\projects\cafe5\db.cafe5.rar
 rar a -df -ma5 -m5 -ep c:\Projects\Cafe5\db.cafe5.rar c:\Projects\Cafe5\db.cafe5.sql
 git add .
 git commit
