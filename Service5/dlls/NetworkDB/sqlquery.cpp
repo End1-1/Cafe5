1930b0aadbc84e8314202f946624e0fb2c16be99
Vasili Kudryashov <end1_1@yahoo.com>
2024-02-09 16:04:50 +0400

    F

--- Service5/dlls/NetworkDB/sqlquery.cpp
+++ Service5/dlls/NetworkDB/sqlquery.cpp
@@ -14,6 +14,7 @@
 void routes(QStringList &r)
 {
     r.append("networkdb");
+    r.append("printservice");
 }
 
 bool networkdb(const QByteArray &indata, QByteArray &outdata, const QHash<QString, DataAddress> &dataMap, const ContentType &contentType)
@@ -37,6 +38,32 @@ bool networkdb(const QByteArray &indata, QByteArray &outdata, const QHash<QStrin
     QJsonObject jo = QJsonDocument::fromJson(getData(indata, dataMap["json"])).object();
     QJsonObject jresponse;
     QueryJsonResponse(s.value("host/host").toString(), jo, jresponse).getResponse();
+
+    return rh.setResponse(HTTP_OK, QJsonDocument(jresponse).toJson(QJsonDocument::Compact));
+}
+
+bool printservice(const QByteArray &indata, QByteArray &outdata, const QHash<QString, DataAddress> &dataMap, const ContentType &contentType)
+{
+    CommandLine cl;
+    QString path;
+    cl.value("dllpath", path);
+    JsonHandler jh;
+    RequestHandler rh(outdata);
+
+    if (contentType != ContentType::ApplilcationJson) {
+        return rh.setDataValidationError("Accept content type: Applilcation/Json");
+    }
+    QString configFile = path + "/networkdb.ini";
+    if (!QFile::exists(configFile)) {
+        LogWriter::write(LogWriterLevel::errors, "", QString("sqlquery config path not exists: %1").arg(configFile));
+        jh["message"] = "Server not configured";
+        return rh.setInternalServerError(jh.toString());
+    }
+    QSettings s(configFile, QSettings::IniFormat);
+    QJsonObject jo = QJsonDocument::fromJson(getData(indata, dataMap["json"])).object();
+    QJsonObject jresponse;
+
+    QString sql = "select ";
 
     return rh.setResponse(HTTP_OK, QJsonDocument(jresponse).toJson(QJsonDocument::Compact));
 }
